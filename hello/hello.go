c78db5f994f21b5e1534e6a949b580834fb4e1f2
gsirois33 <gina.sirois@netapp.com>
2020-05-30 09:21:13 -0600

    Add reverse package.

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gsirois33/first_golang/hello/morestrings"
+)
 
 func main() {
-	fmt.Println("Hello World!!!")
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
 }
