8199f0e24ca2b82cf53c8e3ebba6cdb3dd289f65
gsirois33 <gina.sirois@netapp.com>
2020-05-30 09:42:05 -0600

    Add test for reverse func, use google's go cmt for a thing.

--- hello/hello.go
+++ hello/hello.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"github.com/gsirois33/first_golang/hello/morestrings"
+	"github.com/google/go-cmp/cmp"
 )
 
 func main() {
 	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
